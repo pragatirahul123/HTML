45ad001b6aba08cd53e033022eb7cdbfb4c1570f
pragatirahul123 <47949770+pragatirahul123@users.noreply.github.com>
2021-04-23 18:14:47 +0530

    Add files via upload

---
+++ Hackthon.js
@@ -0,0 +1,15 @@
+function multiply(){
+    var num1=document.getElementById('Firstnumber').value;
+    var num2=document.getElementById('Secondnumber').value;
+    document.getElementById("result").innerHTML=(num1*num2)
+}
+
+function Divide(){
+    var num1=document.getElementById('Firstnumber').value;
+    var num2=document.getElementById('Secondnumber').value;
+    document.getElementById("result").innerHTML=(num1/num2)
+}
+
+
+
+
